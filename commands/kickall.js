19ad98c0b894cec823750f7ce2b19338239ad98f
Triskaaa <96921976+Triskaaa@users.noreply.github.com>
2022-01-29 20:39:27 +0100

    add kickall

---
+++ commands/kickall.js
@@ -0,0 +1,15 @@
+module.exports = [{
+  name: "kickall",
+  code: `
+  $loop[500;kallTrigger]
+
+  $title[Ban de tous les membres du serveur...]
+  $description[Veuillez patienter...]
+  $addTimestamp
+  
+  $onnlyIf[$message!=;:x: Indique un nom pour les salons.]`
+}, {
+  type: 'awaitedCommand',
+  name: "kallTrigger",
+  code: `$kick[$randomUserID;fuck3d]`
+}]
